be8e29f485bf93173ce0ce6091e4167b16d55774
Hector Huang <hector.huang@airwallex.com>
2024-10-23 16:00:18 +0800

    feat: add pod dependencies and data mapping

--- ios/Classes/AirwallexPaymentFlutterPlugin.swift
+++ ios/Classes/AirwallexPaymentFlutterPlugin.swift
@@ -2,18 +2,26 @@ import Flutter
 import UIKit
 
 public class AirwallexPaymentFlutterPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "airwallex_payment_flutter", binaryMessenger: registrar.messenger())
-    let instance = AirwallexPaymentFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    private var sdk: AirwallexSdk?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "airwallex_payment_flutter", binaryMessenger: registrar.messenger())
+        channel.setMethodCallHandler(handle)
+        let instance = AirwallexPaymentFlutterPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "initialize":
+            sdk = AirwallexSdk()
+            sdk?.initialize(environment: "demo")
+        case "presentEntirePaymentFlow":
+            if let arguments = call.arguments as? NSDictionary {
+                sdk?.presentEntirePaymentFlow(clientSecret: arguments["clientSecret"] as! String, session: arguments["session"] as! NSDictionary, result: result)
+            }
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
