859837a61adede12644b66f9e06878208530b346
passy.ruan <passy.ruan@airwallex.com>
2024-10-31 18:09:07 +0800

    chore: upgrade the android sdk

--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 # Change directory to example
 echo "change directory to example"
 cd example
@@ -15,13 +14,7 @@ do
   flutter drive --driver=test_driver/integration_test.dart --target=$test_file
 done
 
-adb -s emulator-5554 emu kill
-
-pkill -f emulator || true
-pkill -f flutter || true
-
-echo "All tests completed."
-
-exit 0
-
-#echo "All tests completed."
+set +e
+pkill -9 -f emulator
+pkill -9 -f flutter
+set -e
