52c805b5f217cfb6d4f302f67c148a019ab08744
passy.ruan <passy.ruan@airwallex.com>
2024-10-31 18:30:01 +0800

    chore: upgrade the android sdk

--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -11,27 +11,34 @@ else
   exit 1
 fi
 
-# Install dependencies
-flutter pub get
+# Initialize a variable to track errors
+error_occurred=false
 
 # Run each test file
 for test_file in integration_test/*.dart; do
   echo "Running Flutter Drive Test for $test_file"
 
-  if ! flutter drive --driver=test_driver/integration_test.dart --target=$test_file --no-build; then
+  # Use a subshell and capture the exit status
+  if ! (flutter drive --driver=test_driver/integration_test.dart --target=$test_file --no-build); then
     echo "Error occurred during testing $test_file"
-    exit 1
+    error_occurred=true
   fi
 
   echo "Finished testing $test_file"
 done
 
-# Handle process cleanup last, errors here will not affect script exit status
-set +e
-pkill -9 -f emulator
-pkill -9 -f flutter
-pkill -9 -f dart
-set -e
+# Conditionally kill processes only if error occurs, to see if it affects the outcome
+if [ "$error_occurred" = true ]; then
+  pkill -9 -f emulator || true
+  pkill -9 -f flutter || true
+  pkill -9 -f dart || true
+fi
+
+# Check if any errors occurred and exit with the appropriate status
+if [ "$error_occurred" = true ]; then
+  echo "One or more tests failed."
+  exit 1
+fi
 
 echo "All tests completed successfully."
 exit 0
