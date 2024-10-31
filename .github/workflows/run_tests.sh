faeb46882e0a04b20fe5f0bb21749d4fb54aaf70
passy.ruan <passy.ruan@airwallex.com>
2024-10-31 18:19:34 +0800

    chore: upgrade the android sdk

--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
+
+echo "AVD is running now."
+adb devices
+
 # Change directory to example
-echo "change directory to example"
-cd example
+if [ -d "example" ]; then
+  cd example
+else
+  echo "Directory 'example' does not exist!"
+  exit 1
+fi
 
 # Install dependencies
-#flutter pub get
+flutter pub get
 
-# Run the integration tests
-echo "Running Flutter Drive"
-for test_file in integration_test/*.dart
-do
+# Run each test file
+for test_file in integration_test/*.dart; do
   echo "Running Flutter Drive Test for $test_file"
-  flutter drive --driver=test_driver/integration_test.dart --target=$test_file
+
+  if ! flutter drive --driver=test_driver/integration_test.dart --target=$test_file --no-build; then
+    echo "Error occurred during testing $test_file"
+    exit 1
+  fi
+
+  echo "Finished testing $test_file"
 done
 
+# Handle process cleanup last, errors here will not affect script exit status
 set +e
-pkill -f emulator
-pkill -f flutter
+pkill -9 -f emulator
+pkill -9 -f flutter
+pkill -9 -f dart
 set -e
+
+echo "All tests completed successfully."
