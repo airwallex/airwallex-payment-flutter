1742c3078aefd3ac996e6dc95195765f22d298e5
passy.ruan <passy.ruan@airwallex.com>
2024-11-01 17:35:52 +0800

    chore: update run_tests.sh

--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 
-set -e
-set -x
-
 echo "AVD is running now."
 adb devices
 
-# Log periodically to ensure the script isn't stuck
-check_interval=60  # Check every 60 seconds
-(
-  while true; do
-    echo "Script still running at $(date)..."
-    sleep $check_interval
-  done
-) &
-
 # Change directory to example
 echo "Attempting to change directory to example"
-if [ -d "example" ]; then
-  cd example
-  ls -al  # This is for debugging to ensure we're in the correct directory
-else
-  echo "Directory 'example' does not exist!"
-  exit 1
-fi
+
+cd example
 
 # Install dependencies
 flutter pub get
@@ -32,12 +15,6 @@ flutter pub get
 echo "Running Flutter Drive Test"
 flutter drive --driver=test_driver/integration_test.dart --target=integration_test/plugin_payment_card_ui_test.dart -d emulator-5554
 
-# Check if any lingering processes are running
-echo "Checking for lingering processes..."
-ps aux | grep emulator
-ps aux | grep flutter
-ps aux | grep dart
-ps aux | grep adb
-
-echo "All tests and processes should be completed successfully."
+#kill $check_pid
+echo "All tests completed successfully."
 exit 0
