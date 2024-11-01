9be71827005465decc96bcdbde6939db5ca771e0
passy.ruan <passy.ruan@airwallex.com>
2024-11-01 12:18:39 +0800

    chore: update run_tests.sh

--- example/test_driver/integration_test.dart
+++ example/test_driver/integration_test.dart
@@ -1,4 +1,6 @@
 import 'package:integration_test/integration_test_driver.dart';
 
 
-Future<void> main() => integrationDriver();
+Future<void> main() async {
+  await integrationDriver(timeout: Duration(minutes: 10));
+}
