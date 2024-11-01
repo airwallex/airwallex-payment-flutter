485c395144d8c8b6c1091c15141ba903edd9969c
passy.ruan <passy.ruan@airwallex.com>
2024-11-01 13:01:29 +0800

    chore: update run_tests.sh

--- .github/workflows/run_tests.sh
+++ .github/workflows/run_tests.sh
@@ -7,20 +7,15 @@ adb devices
 echo "Attempting to change directory to example"
 if [ -d "example" ]; then
   cd example
+  ls -al  # This is for debugging to ensure we're in the correct directory
 else
   echo "Directory 'example' does not exist!"
   exit 1
 fi
 
+# Install dependencies
+flutter pub get
+
 # Run the integration tests
 echo "Running Flutter Drive Test"
-flutter drive --driver=test_driver/integration_test.dart --target=integration_test/plugin_payment_api_test.dart -d emulator-5554
-
-
-#pkill -9 -f emulator || true
-#pkill -9 -f adb || true
-#pkill -9 -f dart || true
-#pkill -9 -f flutter || true
-
-echo "All tests completed successfully."
-exit 0
+flutter drive --driver=test_driver/integration_test.dart --target=integration_test/plugin_payment_card_ui_test.dart -d emulator-5554
