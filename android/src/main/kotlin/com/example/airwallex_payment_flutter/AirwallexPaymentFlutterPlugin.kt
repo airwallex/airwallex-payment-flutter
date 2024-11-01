a1fc48b1e4135a9d727499eb98f801533769dae9
passy.ruan <passy.ruan@airwallex.com>
2024-11-01 11:29:18 +0800

    chore: create plugin_payment_ui_test.dart

--- android/src/main/kotlin/com/example/airwallex_payment_flutter/AirwallexPaymentFlutterPlugin.kt
+++ android/src/main/kotlin/com/example/airwallex_payment_flutter/AirwallexPaymentFlutterPlugin.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.content.Context
 import androidx.activity.ComponentActivity
 import com.airwallex.android.core.log.AirwallexLogger
+import com.example.airwallex_payment_flutter.util.AirwallexActivityManager
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -11,60 +12,72 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 
+
 /** AirwallexPaymentFlutterPlugin */
-class AirwallexPaymentFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
-  private lateinit var channel: MethodChannel
-  private lateinit var applicationContext: Context
-  private var activity: ComponentActivity? = null
-  private var sdkModule = AirwallexPaymentSdkModule()
+class AirwallexPaymentFlutterPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    private lateinit var channel: MethodChannel
+    private lateinit var applicationContext: Context
+    private var activity: ComponentActivity? = null
+    private var sdkModule = AirwallexPaymentSdkModule()
+    private var activityManager: AirwallexActivityManager? = null
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    applicationContext = flutterPluginBinding.applicationContext
-    channel = MethodChannel(
-      flutterPluginBinding.binaryMessenger,
-      "samples.flutter.dev/airwallex_payment"
-    )
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-    AirwallexLogger.info("AirwallexPaymentFlutterPlugin: onMethodCall call.method= ${call.method}")
-    activity?.let {
-      when (call.method) {
-        //initialize
-        "initialize" -> sdkModule.initialize(applicationContext as Application, call, result)
-        //ui
-        "presentEntirePaymentFlow" -> sdkModule.presentEntirePaymentFlow(it, call, result)
-        "presentCardPaymentFlow" -> sdkModule.presentCardPaymentFlow(it, call, result)
-        //low-level
-        "startPayWithCardDetails" -> sdkModule.startPayWithCardDetails(it, call, result)
-        "startGooglePay" -> sdkModule.startGooglePay(it, call, result)
-        else -> {
-          result.notImplemented()
-        }
-      }
-    } ?: run {
-      result.error("Activity not attached", "Unable to perform operation", null)
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        applicationContext = flutterPluginBinding.applicationContext
+        activityManager = AirwallexActivityManager(applicationContext as Application)
+        channel = MethodChannel(
+            flutterPluginBinding.binaryMessenger,
+            "samples.flutter.dev/airwallex_payment"
+        )
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        AirwallexLogger.info("AirwallexPaymentFlutterPlugin: onMethodCall call.method= ${call.method}")
+        activity?.let {
+            when (call.method) {
+                //initialize
+                "initialize" -> {
+                    sdkModule.initialize(applicationContext as Application, call, result)
+                }
+                //ui
+                "presentEntirePaymentFlow" -> sdkModule.presentEntirePaymentFlow(it, call, result)
+                "presentCardPaymentFlow" -> sdkModule.presentCardPaymentFlow(it, call, result)
+                //low-level
+                "startPayWithCardDetails" -> sdkModule.startPayWithCardDetails(it, call, result)
+                "startGooglePay" -> sdkModule.startGooglePay(it, call, result)
+                "closeNativeScreen" -> {
+                    activityManager?.getCurrentActivity()?.finish()
+                    result.success(null)
+                }
 
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    activity = binding.activity as ComponentActivity
-  }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        } ?: run {
+            result.error("Activity not attached", "Unable to perform operation", null)
+        }
+    }
 
-  override fun onDetachedFromActivity() {
-    activity = null
-  }
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 
-  override fun onDetachedFromActivityForConfigChanges() {
-    onDetachedFromActivity()
-  }
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity as ComponentActivity
+    }
 
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    onAttachedToActivity(binding)
-  }
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        onDetachedFromActivity()
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        onAttachedToActivity(binding)
+    }
+
+
 }
---
+++ example/integration_test/plugin_payment_ui_test.dart
@@ -0,0 +1,78 @@
+import 'package:airwallex_payment_flutter_example/main.dart' as app;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Test multiple payment flows', (WidgetTester tester) async {
+    await testStartPayWithCardDetails(tester);
+    await testPresentCardPaymentFlow(tester);
+    await closeNativeScreenAndSettle(tester);
+    await testPresentEntirePaymentFlow(tester);
+  });
+}
+
+Future<void> testStartPayWithCardDetails(WidgetTester tester) async {
+  app.main();
+  await tester.pumpAndSettle();
+
+  final startPayButton = find.text('startPayWithCardDetails');
+  await tester.tap(startPayButton);
+  await tester.pumpAndSettle();
+
+  final successTextFinder = find.text('success');
+  expect(successTextFinder, findsOneWidget,
+      reason: 'The dialog should show "success" as the message.');
+
+  final closeSuccessDialogButton = find.text('OK');
+  await tester.tap(closeSuccessDialogButton);
+  await tester.pumpAndSettle();
+}
+
+Future<void> testPresentCardPaymentFlow(WidgetTester tester) async {
+  app.main();
+  await tester.pumpAndSettle();
+
+  final presentCardFlowButton = find.text('presentCardPaymentFlow');
+  await tester.tap(presentCardFlowButton);
+  await tester.pumpAndSettle();
+
+  final dialog = find.byType(AlertDialog);
+  expect(dialog, findsNothing,
+      reason: 'Dialog should not be shown for valid parameters.');
+}
+
+Future<void> testPresentEntirePaymentFlow(WidgetTester tester) async {
+  app.main();
+  await tester.pumpAndSettle();
+
+  final entirePaymentFlowButton = find.text('presentEntirePaymentFlow');
+  await tester.tap(entirePaymentFlowButton);
+  await tester.pumpAndSettle();
+
+  final dialog = find.byType(AlertDialog);
+  expect(dialog, findsNothing,
+      reason: 'Dialog should not be shown for valid parameters.');
+}
+
+const platform = MethodChannel('samples.flutter.dev/airwallex_payment');
+
+Future<void> closeNativeScreenAndSettle(WidgetTester tester) async {
+  await closeNativeScreen();
+  app.main();
+  await tester.pumpAndSettle();
+  final closeSuccessDialogButton = find.text('OK');
+  await tester.tap(closeSuccessDialogButton);
+  await tester.pumpAndSettle();
+}
+
+Future<void> closeNativeScreen() async {
+  try {
+    await platform.invokeMethod('closeNativeScreen');
+  } on PlatformException catch (e) {
+    print("Failed to close native screen: '${e.message}'.");
+  }
+}
