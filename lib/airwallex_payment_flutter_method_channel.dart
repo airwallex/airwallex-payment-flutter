24cef5bf4a3cd6e2a15f072d9ec01d2d803852cc
weiping-awx <weiping.li@airwallex.com>
2026-03-17 17:51:05 +0800

    ci: use the lowest supported version in CI

--- lib/airwallex_payment_flutter_method_channel.dart
+++ lib/airwallex_payment_flutter_method_channel.dart
@@ -80,11 +80,12 @@ class MethodChannelAirwallexPaymentFlutter
   @override
   void setTintColor(Color color) {
     if (Platform.isIOS) {
+      // ignore: deprecated_member_use
       methodChannel.invokeMethod('setTintColor', {
-        'red': (color.r * 255.0).round() & 0xff,
-        'green': (color.g * 255.0).round() & 0xff,
-        'blue': (color.b * 255.0).round() & 0xff,
-        'alpha': (color.a * 255.0).round() & 0xff,
+        'red': color.red,
+        'green': color.green,
+        'blue': color.blue,
+        'alpha': color.alpha,
       });
     }
   }
